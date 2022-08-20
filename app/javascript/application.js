4712eb0a81d1a5fd595db6446f2c28d3d740e18a
Hai Nguyen <hai.nguyen@employmenthero.com>
2022-08-21 00:13:09 +0700

    Remove hotwire and stimulus not used

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "components/hello_react"
+
+// TODO: remove
+// import "@hotwired/turbo-rails"
+// import "controllers"
+//
+
 import "bootstrap"
+import "components/hello_react"
