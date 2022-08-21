6dfaa666bdc57d8463c6cebe881fd4e1388e6e63
Hai Nguyen <hai.nguyen@employmenthero.com>
2022-08-21 16:44:49 +0700

    Remove outupdated files

--- app/javascript/application.js
+++
@@ -1,9 +0,0 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-
-// TODO: remove
-// import "@hotwired/turbo-rails"
-// import "controllers"
-//
-
-import "bootstrap"
-import "components/hello_react"
