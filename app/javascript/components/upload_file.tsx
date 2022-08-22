69984b086f0667782f6ce6536e0c857c7768ab78
Kevin Vo <vodangkhoa@gmail.com>
2022-08-22 16:09:06 +0700

    [f] - file upload

--- app/javascript/components/upload_file.tsx
+++ app/javascript/components/upload_file.tsx
@@ -1,49 +1,23 @@
 import React, { useState, Component } from "react"
 import axios from 'axios';
 
-
 function FileUploader() {
-// class FileUploader extends Component {
-  const [selectedFile, setSelectedFile] = useState(null)
-
-  const onFileChange = event => {
-    setSelectedFile(event.target.files[0])
-  };
-
-  const onFileUpload = () => {
+  const onFileUpload = (event) => {
+    const file = event.target.files[0]
     const formData = new FormData();
     formData.append(
       "myFile",
-      this.state.selectedFile,
-      this.state.selectedFile.name
+      file,
+      file.name
     );
-    console.log(this.state.selectedFile);
-    axios.post("api/uploadfile", formData);
-  };
-
-  const fileData = () => {
-    if (selectedFile) {
-      return (
-        <div>
-          <h2>File Details:</h2>
-          <p>File Name: {selectedFile.name}</p>
-          <p>File Type: {selectedFile.type}</p>
-          <p>
-            Last Modified:{" "}
-            {selectedFile.lastModifiedDate.toDateString()}
-          </p>
-
-        </div>
-      );
-    }
+    axios.post("/api/upload_file", formData);
   };
 
   return (
     <div className="d-flex flex-row justify-content-center align-items-center" style={{height: "500px"}}>
       <div>
-        <input type="file" onChange={this.onFileChange} />
+        <input type="file" accept=".csv" onChange={onFileUpload} />
       </div>
-      {fileData()}
     </div>
   );
 }
