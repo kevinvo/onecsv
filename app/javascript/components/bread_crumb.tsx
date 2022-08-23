437755ebb50f6df526fe6008b2dc307eb33492a6
Kevin Vo <vodangkhoa@gmail.com>
2022-08-23 18:56:09 +0700

    [f] - make select a file a link for now

--- app/javascript/components/bread_crumb.tsx
+++ app/javascript/components/bread_crumb.tsx
@@ -7,7 +7,7 @@ const BreadCrumb = (props) => {
         <h6>Import a file</h6>
         <nav aria-label="breadcrumb">
           <ol className="breadcrumb">
-            <li className="breadcrumb-item active" aria-current="page">Select a file</li>
+            <li className="breadcrumb-item active" aria-current="page"><a href="/">Select a file</a></li>
             <li className="breadcrumb-item"><a href="/select-column-headers">Select column headers</a></li>
             <li className="breadcrumb-item"><a href="#">Map template columns</a></li>
             <li className="breadcrumb-item"><a href="#">Clean and finalize</a></li>
