01e1b5892310d000e7ea6be9be4b4cc1fa5084d0
Hai Nguyen <hai.nguyen@employmenthero.com>
2022-08-25 00:37:29 +0700

    Add map_headers component

---
+++ app/javascript/components/map_headers.tsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react"
+import BreadCrumb from "./bread_crumb"
+
+function MapHeaders() {
+	return (
+		<>
+			<BreadCrumb>
+				<div className="d-flex flex-row justify-content-center align-items-center">
+          <table className="table table-bordered">
+            <thead>
+            </thead>
+            <tbody>
+            </tbody>
+            <tfoot>
+            </tfoot>
+          </table>
+        </div>
+			</BreadCrumb>
+		</>
+	)
+}
+
+export default MapHeaders
