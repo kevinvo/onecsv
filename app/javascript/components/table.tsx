a0037f5b6bc5470661dd3fb5f5537fd4ef9f2514
Kevin Vo <vodangkhoa@gmail.com>
2022-08-25 11:06:44 +0700

    [f] - use data grid

--- app/javascript/components/table.tsx
+++ app/javascript/components/table.tsx
@@ -1,22 +1,21 @@
 import React, { useState, useEffect, useMemo, useRef, HTMLProps } from "react"
-import { useTable } from 'react-table'
+import DataGrid from 'react-data-grid';
 
 const Table = (props) => {
-  const [data, setData] = useState([])
-  const [headers, setHeaders] = useState([])
+  const [rows, setRows] = useState([])
+  const [columns, setColumns] = useState([])
 
   useEffect(() => {
     if (props.rows && props.rows.length > 0) {
       const rowData = props.rows.slice(1).map(row => {
         const obj = {}
         row.forEach(function (element, index) {
-          const colIndex = `col${index}`
-          obj[colIndex] = element
+          obj[`col${index}`] = element
           obj["id"] = element
         })
         return obj
       })
-      setData(rowData)
+      setRows(rowData)
     }
   }, [props.rows])
 
@@ -24,57 +23,17 @@ const Table = (props) => {
     if (props.rows && props.rows.length > 0) {
       const headers = props.rows[0].map((element, index) => {
         return {
-          Header: element,
-          accessor: `col${index}`
+          name: element,
+          key: `col${index}`
         }
       })
-      setHeaders(headers)
+      setColumns(headers)
     }
   }, [props.rows])
-
-  function MyTable({ columns, data }) {
-    const {
-      getTableProps,
-      getTableBodyProps,
-      headerGroups,
-      rows,
-      prepareRow,
-    } = useTable({
-      columns,
-      data,
-    })
-
-    return (
-      <table {...getTableProps()}>
-        <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-            ))}
-          </tr>
-        ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-              })}
-            </tr>
-          )
-        })}
-        </tbody>
-      </table>
-    )
-  }
-
-  const table = headers.length > 0 ? (<MyTable columns={headers} data={data} />) : null
-
+  
   return (
-    table
+    columns.length > 0 ?
+      (<DataGrid columns={columns} rows={rows} className="fill-grid" defaultColumnOptions={{sortable: true, resizable: true}} />) : null
   )
 }
 
