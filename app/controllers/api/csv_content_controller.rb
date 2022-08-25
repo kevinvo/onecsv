d50122e5b1e952bc96af6569e8155a11e253d5e6
Hai Nguyen <hai.nguyen@employmenthero.com>
2022-08-25 18:04:37 +0700

    Fix invalid_byte_sequence_utf8

--- app/controllers/api/csv_content_controller.rb
+++ app/controllers/api/csv_content_controller.rb
@@ -5,7 +5,7 @@ class Api::CsvContentController < ApiController
   def index
     uploaded_file_path = session[:uploaded_file_path]
     lines = [].tap do |lines|
-      File.open(uploaded_file_path, "r").first(40).each.with_index(1) do |line|
+      File.open(uploaded_file_path, "r:ISO-8859-1:UTF-8").first(40).each.with_index(1) do |line|
         per_line = line.split(/\t/)[0]
         values = CSV.parse(per_line)
         lines << values[0]
