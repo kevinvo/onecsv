32d25e2b854fec5d9ba8c1de23534fa0fb01cddb
Kevin Vo <vodangkhoa@gmail.com>
2022-09-05 15:21:24 +0700

    [f] - data types

--- app/services/csv_data_type_service.rb
+++ app/services/csv_data_type_service.rb
@@ -1,5 +1,9 @@
+require "monetize"
+require "money"
+
 class CsvDataTypeService < ApplicationService
 
+  ACCURACY = 1
   attr_reader :values
 
   def initialize(values)
@@ -7,6 +11,33 @@ class CsvDataTypeService < ApplicationService
   end
 
   def call
-    DataTypeConstant::EMAIL
+    if self.is_email
+      DataTypeConstant::EMAIL
+    elsif self.is_currency
+      DataTypeConstant::CURRENCY
+    elsif self.is_integer or self.is_float
+      DataTypeConstant::NUMBER
+    else
+      DataTypeConstant::TEXT
+    end
   end
+
+  private
+  def is_email
+    @values.map {|value| URI::MailTo::EMAIL_REGEXP.match value}.select(&:itself).length > ACCURACY
+  end
+
+  def is_currency
+    currency_values = @values.map {|amount| Money::Currency.analyze(amount).length > 0 ? amount : nil}.select(&:itself)
+    currency_values.map {|amount| Monetize.parse(amount)}.length > ACCURACY
+  end
+
+  def is_integer
+    @values.map {|value| Integer(value, exception: false)}.select(&:itself).length > ACCURACY
+  end
+
+  def is_float
+    @values.map {|value| Float(value, exception: false)}.select(&:itself).length > ACCURACY
+  end
+
 end
