cefea53d29cff01ba25969c13b40449d7e23643d
Kevin Vo <vodangkhoa@gmail.com>
2022-09-14 14:25:38 +0700

    [f] - update header scope

--- app/models/header.rb
+++ app/models/header.rb
@@ -2,4 +2,8 @@ class Header < ApplicationRecord
   belongs_to :template
 
   enum data_type: %i[date currency email number text]
+  
+  scope :required, -> { where(is_required_field: true ) }
+  scope :optional, -> { where(is_required_field: false ) }
+
 end
