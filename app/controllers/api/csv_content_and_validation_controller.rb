c82069f9e448fccd0dfc222b0969852433d68459
Kevin Vo <vodangkhoa@gmail.com>
2022-09-17 22:24:12 +0700

    [f] - read column values from DB

--- app/controllers/api/csv_content_and_validation_controller.rb
+++ app/controllers/api/csv_content_and_validation_controller.rb
@@ -3,35 +3,23 @@ require 'csv'
 class Api::CsvContentAndValidationController < ApiController
 
   def index
-    uploaded_file_path = session[:uploaded_file_path]
-    csv = CSV.read(uploaded_file_path, :headers=>true, encoding: CsvConstant::ENCODING)
     template = current_user.templates.last
-
     header_map = template.headers.map do |header|
       { header_name: header.name.to_s.strip,
         data_type: header.read_attribute_before_type_cast(:data_type),
         required: header.is_required_field }
     end
 
-    look_up_via_header_name = header_map.map { |header| [header[:header_name], header] }.to_h
-    csv = CSV.read(uploaded_file_path, :headers=>true, encoding: CsvConstant::ENCODING)
-
-    rows = csv.map do |csv_objects|
-      csv_objects.map do |csv_obj|
-        header_name = csv_obj.first.to_s.strip
-        column_value = csv_obj.last.to_s.strip
-
-        data_type = look_up_via_header_name[header_name][:data_type]
-        is_required_field = look_up_via_header_name[header_name][:required]
+    rows = template.headers.map do |header|
+      header.csv_columns.map do |column_value|
         type_validator_obj = TypeValidatorService.new(column_value=column_value,
-                                                      data_type=data_type,
-                                                      is_required_field=is_required_field).is_valid
-
+                                                      data_type=header.read_attribute_before_type_cast(:data_type),
+                                                      is_required_field=header.is_required_field).is_valid
         { value: column_value,
           data_type: data_type,
           error: type_validator_obj.error_message }
       end
-    end
+    end.transpose
 
     data = {headers: header_map, rows: rows}
     msg = {:status => :ok, :data => data}
