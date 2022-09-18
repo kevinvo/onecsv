33b772432816c64426a9f0274a9edf9fab7b673d
Kevin Vo <vodangkhoa@gmail.com>
2022-09-18 11:52:22 +0700

    [f] - remove select column headers code

--- app/typescript/app.tsx
+++ app/typescript/app.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import SelectFile from './components/select_file'
-import SelectColumnHeader from './components/select_column_headers'
 import MapTemplateColumn from './components/map_template_column'
 import CleanAndFinalize from './components/clean_and_finalize'
 
@@ -10,7 +9,6 @@ export default function App() {
     <div className='App'>
       <Routes>
         <Route path='/' element={<SelectFile />} />
-        <Route path='/select-column-headers' element={<SelectColumnHeader />} />
         <Route path='/map-template-columns' element={<MapTemplateColumn />} />
         <Route path='/clean-and-finalize' element={<CleanAndFinalize />} />
       </Routes>
