c6efcdb2968e84376cea8d357eb4fda1c90f3733
Kevin Vo <vodangkhoa@gmail.com>
2022-09-21 17:09:15 +0700

    [f] - remove header

--- app/typescript/components/auto_hide_toast.tsx
+++ app/typescript/components/auto_hide_toast.tsx
@@ -10,14 +10,6 @@ export default function AutohideToast(props) {
       <Col xs={6}>
         <ToastContainer position="top-end" className="p-3">
           <Toast bg='success' onClose={() => props.setShowToast(false)} show={props.showToast} delay={2000} autohide>
-            <Toast.Header>
-              <img
-                src="holder.js/20x20?text=%20"
-                className="rounded me-2"
-                alt=""
-              />
-              <strong className="me-auto"></strong>
-            </Toast.Header>
             <Toast.Body>{props.message}</Toast.Body>
           </Toast>
         </ToastContainer>
