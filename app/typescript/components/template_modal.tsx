9991ddae0f934d914025fd7672b57aa8bf3dff0a
Hai Nguyen <hai.nguyen@employmenthero.com>
2022-09-22 22:04:14 +0700

    Update UI to show list of templates

--- app/typescript/components/template_modal.tsx
+++ app/typescript/components/template_modal.tsx
@@ -7,9 +7,25 @@ import { useNavigate } from 'react-router-dom'
 
 const TemplateModal = ({ templateName, onInputTemplateName, ...props }) => {
   const [show, setShow] = useState(false)
+  const [templates, setTemplates] = useState([])
   const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
+  const handleShow = () => {
+    setShow(true)
+    fetchListTemplates()
+  }
+
   const navigate = useNavigate()
+
+  const fetchListTemplates = () => {
+    axios
+      .get('/api/csv_template')
+      .then(response => {
+        setTemplates(response.data.templates)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
 
   const onSaveTemplateName = () => {
     const data = {
@@ -59,12 +75,23 @@ const TemplateModal = ({ templateName, onInputTemplateName, ...props }) => {
           <Form>
             <Form.Group className='mb-3' controlId='formBasicName'>
               <Form.Label>Template Name</Form.Label>
-              <Form.Control
-                type='text'
-                placeholder='Enter name'
-                value={templateName}
-                onChange={(event) => onInputTemplateName(event)}
-              />
+              { templates.length > 0 ? (
+                <Form.Select aria-label="Default select template_name">
+                  <option>Open this select the template</option>
+                  {
+                    templates.map((template, index) => (
+                      <option value={template.id}>{template.name}</option>
+                    ))
+                  }
+                </Form.Select>
+              ) : (
+                <Form.Control
+                    type='text'
+                    placeholder='Enter name'
+                    value={templateName}
+                    onChange={(event) => onInputTemplateName(event)}
+                />
+              )}
             </Form.Group>
           </Form>
         </Modal.Body>
