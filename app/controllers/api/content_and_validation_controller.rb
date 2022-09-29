20deedd5865f5174fa4d2f07ba8abb290f4e2634
Kevin Vo <vodangkhoa@gmail.com>
2022-09-29 22:46:17 +0700

    [f] - save error message into lookup table

--- app/controllers/api/content_and_validation_controller.rb
+++ app/controllers/api/content_and_validation_controller.rb
@@ -7,13 +7,17 @@ module Api
     def index
       template_id = session[:template_id]
       template = Template.includes(template_headers: :header).find_by(id: template_id, user: current_user)
+
+      column_value_error_message_lookup = {}
       header_map = template.template_headers.map do |template_header|
         header = template_header.header
         data_type = header.read_attribute_before_type_cast(:data_type)
-        total_errors = template_header.column_values.map do |column_value|
+        total_errors = template_header.column_values.each_with_index.map do |column_value, index|
           type_validator_obj = TypeValidatorService.new(column_value,
                                                         data_type,
                                                         header.is_required_field).valid?
+
+          column_value_error_message_lookup[header.id.to_s + index.to_s] = type_validator_obj.error_message
           type_validator_obj.error_message.empty? ? 0 : 1
         end.sum
 
@@ -24,16 +28,15 @@ module Api
       end
 
       rows = template.template_headers.map do |template_header|
-        template_header.column_values.map do |column_value|
+        template_header.column_values.each_with_index.map do |column_value, index|
           column_value = column_value.to_s.strip
           header = template_header.header
           data_type = header.read_attribute_before_type_cast(:data_type)
-          type_validator_obj = TypeValidatorService.new(column_value,
-                                                        data_type,
-                                                        header.is_required_field).valid?
+          error_message = column_value_error_message_lookup[header.id.to_s + index.to_s]
+
           { value: column_value,
             data_type: data_type,
-            error: type_validator_obj.error_message }
+            error: error_message }
         end
       end.transpose
 
