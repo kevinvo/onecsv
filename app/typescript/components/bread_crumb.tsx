3c5387d1109608ada00d9b15b6637e5583896667
Hai Nguyen <hai.nguyen@employmenthero.com>
2022-09-29 23:54:09 +0700

    Fix breadcrumb

--- app/typescript/components/bread_crumb.tsx
+++ app/typescript/components/bread_crumb.tsx
@@ -1,26 +1,28 @@
 import React from 'react'
+import Breadcrumb from 'react-bootstrap/Breadcrumb'
 
 export default function BreadCrumb(props) {
+  const items = [
+    { href: '/', name: 'Import a file' },
+    { href: '/map-template-columns', name: 'Map template columns' },
+    { href: '/clean-and-finalize', name: 'Clean and finalize' },
+  ];
+
   return (
     <>
       <div style={{ margin: '60px' }}>
-        <div className='d-flex justify-content-between'>
-          <div>Import a file</div>
-        </div>
-
-        <nav aria-label='breadcrumb'>
-          <ol className='breadcrumb'>
-            <li className='breadcrumb-item active' aria-current='page'>
-              <a href='/'>Select a file</a>
-            </li>
-            <li className='breadcrumb-item'>
-              <a href='/map-template-columns'>Map template columns</a>
-            </li>
-            <li className='breadcrumb-item'>
-              <a href='/clean-and-finalize'>Clean and finalize</a>
-            </li>
-          </ol>
-        </nav>
+        <Breadcrumb>
+          {items.map((item, index) =>
+            item.href === props.location_path ? (
+              <Breadcrumb.Item key={0} href={item.href} active>{item.name}
+              </Breadcrumb.Item>
+            ) : (
+              <Breadcrumb.Item key={index+1} href={item.href}>
+                {item.name}
+              </Breadcrumb.Item>
+            )
+          )}
+        </Breadcrumb>
         {props.children}
       </div>
     </>
