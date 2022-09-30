e9dc2f3d84263fdadff5e136be942f964677205f
Kevin Vo <vodangkhoa@gmail.com>
2022-09-30 08:44:26 +0700

    [f] - hotjar

--- app/typescript/app.tsx
+++ app/typescript/app.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Routes, Route } from 'react-router-dom'
 import SelectFile from './pages/select_file'
 import MapTemplateColumn from './pages/map_template_column'
 import CleanAndExport from './pages/clean_and_export'
+import { hotjar } from 'react-hotjar'
 
 export default function App() {
+
+  useEffect(() => {
+    hotjar.initialize(3181232, 6)
+  }, [])
+
   return (
     <div className='App'>
       <Routes>
