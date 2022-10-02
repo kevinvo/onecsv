2f5c6443ca2f0f6b2ccd93ba920182b6493cb773
Hai Nguyen <hai.nguyen@employmenthero.com>
2022-10-02 07:42:58 +0700

    Refactor content_and_validation_controller

--- app/controllers/api/content_and_validation_controller.rb
+++ app/controllers/api/content_and_validation_controller.rb
@@ -5,27 +5,9 @@ require 'csv'
 module Api
   class ContentAndValidationController < ApiController
     def index
-      template_id = session[:template_id]
-      template = Template.by_template_and_user(template_id, current_user)
-
-      column_value_error_message_lookup = {}
-      header_map = template.template_headers.map do |template_header|
-        header = template_header.header
-        data_type = header.read_attribute_before_type_cast(:data_type)
-        total_errors = template_header.column_values.each_with_index.map do |column_value, index|
-          type_validator_obj = TypeValidatorService.new(column_value,
-                                                        data_type,
-                                                        header.is_required_field).valid?
-
-          column_value_error_message_lookup[header.id.to_s + index.to_s] = type_validator_obj.error_message
-          type_validator_obj.error_message.empty? ? 0 : 1
-        end.sum
-
-        { header_name: header.name,
-          data_type: header.read_attribute_before_type_cast(:data_type),
-          required: header.is_required_field,
-          total_errors: total_errors }
-      end
+      header_mapping_service = HeaderMappingService.new(template, column_value_error_message_lookup)
+      header_mapping_service.call
+      header_map = header_mapping_service.data
 
       rows = template.template_headers.map do |template_header|
         template_header.column_values.each_with_index.map do |column_value, index|
@@ -42,6 +24,17 @@ module Api
 
       msg = { status: :ok, headers: header_map, rows: rows, template: template }
       render json: msg
+    end
+
+    private
+
+    def template
+      template_id = session[:template_id]
+      @template ||= Template.by_template_and_user(template_id, current_user)
+    end
+
+    def column_value_error_message_lookup
+      @column_value_error_message_lookup ||= {}
     end
   end
 end
